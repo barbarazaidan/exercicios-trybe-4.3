a97302882bbf0e2e121404815fa0af26dd8e315c
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-08-25 19:58:02 -0300

    Fiz o exercício da menor e maior palavra

---
+++ exercicio-3.js
@@ -0,0 +1,21 @@
+// retornar maior e menor palavra de um array
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
